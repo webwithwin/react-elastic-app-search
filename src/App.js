277197e1c6467c116a65459c39c94bd1fccdd678
Winardi Winardi <winardi@gdn-commerce.com>
2021-02-07 14:54:12 +0700

    add boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
+import { SearchProvider, Results, SearchBox } from '@elastic/react-search-ui';
+import { Layout } from '@elastic/react-search-ui-views';
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+
+const connector = new AppSearchAPIConnector({
+  searchKey: process.env.REACT_APP_SEARCH_KEY,
+  engineName: process.env.REACT_APP_ENGINE_NAME,
+  endpointBase: process.env.REACT_APP_ENDPOINT_BASE,
+  cacheResponses: false
+})
+
+const config = {
+  apiConnector: connector,
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchProvider
+      config={config}
+    >
+      <div className="App">
+        <Layout
+          header={<SearchBox />}
+          bodyContent={<Results titleField="title" urlField="nps_link" />}
+        />
+      </div>
+    </SearchProvider>
   );
 }
 
